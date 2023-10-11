7a6c2ace68aa4c5aef73b9b74e49b6490c53bb9d
= <=>
2023-10-12 02:34:45 +0300

    :(

--- src/lab1/calculator.py
+++ src/lab1/calculator.py
@@ -1,8 +1,9 @@
-"""Calculator"""  
+"""Calculator"""
 
 import re
+from typing import Any, Dict
 
-env = {}
+env: Dict[str, Any] = {}
 env["locals"] = None
 env["globals"] = None
 env["__name__"] = None
@@ -10,20 +11,20 @@ env["__file__"] = None
 env["__builtins__"] = None
 
 
-def calculator(input_string):
+def calculator(input_string: str):
     """calculate function"""
-    if not re.search('[a-zA-Z=@!#$&^~`\'\":;<>,]', input_string):
+    if not re.search("[a-zA-Z=@!#$&^~`'\":;<>,]", input_string):
         try:
             result = eval(input_string, env)
             return result
         except ZeroDivisionError:
-            return 'You can not divide by zero'
+            return "You can not divide by zero"
         except SyntaxError:
-            return 'Please enter the correct expression'
-    return 'You used forbidden symbols'
+            return "Please enter the correct expression"
+    return "You used forbidden symbols"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     user_input = input("enter expression:")
     output = calculator(user_input)
     print(output)
