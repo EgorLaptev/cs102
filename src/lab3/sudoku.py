1c66ee68677531f6f07316e5c3c6df7477df2013
Egor Laptev <egor.lapteffff@gmail.com>
2023-10-29 14:20:29 +0300

    the check_solution function was written

--- src/lab3/sudoku.py
+++ src/lab3/sudoku.py
@@ -150,9 +150,32 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
 
 
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
-    """ Если решение solution верно, то вернуть True, в противном случае False """
-    # TODO: Add doctests with bad puzzles
-    pass
+    """ Если решение solution верно, то вернуть True, в противном случае False
+    >>> grid = [ \
+            ['5', '3', '4', '6', '7', '8', '9', '1', '2'], \
+            ['6', '7', '2', '1', '9', '5', '3', '4', '8'], \
+            ['1', '9', '8', '2', '4', '2', '5', '6', '7'], \
+            ['8', '5', '9', '7', '6', '1', '4', '2', '3'], \
+            ['4', '2', '6', '8', '5', '3', '7', '9', '1'], \
+            ['7', '1', '3', '9', '2', '4', '8', '5', '6'], \
+            ['9', '6', '1', '5', '3', '7', '2', '8', '4'], \
+            ['2', '8', '7', '4', '1', '9', '6', '3', '5'], \
+            ['3', '4', '5', '2', '8', '6', '1', '7', '9'] \
+        ]
+    >>> check_solution(grid)
+    False
+    """
+
+    n = len(solution)
+    rows = [ set(row) for row in solution ]
+    cols = [ set(get_col(solution, (0, col))) for col in range(n) ]
+    blocks = [ set(get_block(solution, (row, col))) for row in range(0, n, 3) for col in range(0, n, 3) ]
+    
+    uniq_rows = all([ len(row) == n for row in rows ])
+    uniq_cols = all([ len(col) == n for col in cols ])
+    uniq_blocks = all([ len(block) == n for block in blocks ])
+
+    return uniq_rows and uniq_cols and uniq_blocks
 
 
 def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
@@ -180,7 +203,7 @@ def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
 
 
 if __name__ == "__main__":
-    for fname in ["puzzle1.txt", "puzzle2.txt", "puzzle3.txt"]:
+    for fname in ["puzzle1.txt"]:
         grid = read_sudoku(fname)
         display(grid)
         solution = solve(grid)
--- tests/lab3/test_sudoku.py
+++ tests/lab3/test_sudoku.py
@@ -10,6 +10,7 @@ class TestSudoku(unittest.TestCase):
         self.get_col = get_col
         self.get_block = get_block
         self.read_sudoku = read_sudoku
+        self.check_solution = check_solution
 
     def test_group(self):
         self.assertEqual(group([1, 2, 3, 4], 2), [[1, 2], [3, 4]])
@@ -48,3 +49,25 @@ class TestSudoku(unittest.TestCase):
         grid = read_sudoku('src/lab3/puzzle1.txt')
         self.assertEqual(find_possible_values(grid, (0,2)), {'1', '2', '4'})
         self.assertEqual(find_possible_values(grid, (4,7)), {'2', '5', '9'})
+
+    def test_check_solution(self):
+        self.assertEqual(check_solution([
+            ['5', '3', '4', '6', '7', '8', '9', '1', '2'], 
+            ['6', '7', '2', '1', '9', '5', '3', '4', '8'], 
+            ['1', '9', '8', '3', '4', '2', '5', '6', '7'], 
+            ['8', '5', '9', '7', '6', '1', '4', '2', '3'], 
+            ['4', '2', '6', '8', '5', '3', '7', '9', '1'], 
+            ['7', '1', '3', '9', '2', '4', '8', '5', '6'], 
+            ['9', '6', '1', '5', '3', '7', '2', '8', '4'], 
+            ['2', '8', '7', '4', '1', '9', '6', '3', '5'], 
+            ['3', '4', '5', '2', '8', '6', '1', '7', '9']]), True)
+        self.assertEqual(check_solution([
+            ['5', '3', '4', '6', '7', '8', '9', '1', '2'], 
+            ['6', '7', '2', '1', '9', '5', '3', '4', '8'], 
+            ['1', '9', '8', '2', '4', '2', '5', '6', '7'], 
+            ['8', '5', '9', '7', '6', '1', '4', '2', '3'], 
+            ['4', '2', '6', '8', '5', '3', '7', '9', '1'], 
+            ['7', '1', '3', '9', '2', '4', '8', '5', '6'], 
+            ['9', '6', '1', '5', '3', '7', '2', '8', '4'], 
+            ['2', '8', '7', '4', '1', '9', '6', '3', '5'], 
+            ['3', '4', '5', '2', '8', '6', '1', '7', '9']]), False)
