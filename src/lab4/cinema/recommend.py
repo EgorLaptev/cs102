aee4641b23bd15c8496c05c01a0b43b9e73fc049
Egor Laptev <egor.lapteffff@gmail.com>
2023-11-01 15:38:10 +0300

    if no recommendations

--- src/lab4/cinema/recommend.py
+++ src/lab4/cinema/recommend.py
@@ -5,20 +5,6 @@
 
 from typing import List, Dict, Set
 
-user_history = input().split(',')
-
-
-# get the history of others
-with open('data/history.txt', 'r', encoding='UTF-8') as history_file:
-    history = [ user.split(',') for user in history_file.read().splitlines() ]
-
-
-# get the movies
-with open('data/movies.txt', 'r', encoding='UTF-8') as movies_file:
-    all_movies = [ movie.split(',') for movie in movies_file.read().splitlines() ]
-    all_movies = { movie[0]:movie[1] for movie in all_movies }
-
-
 def get_views(movies: Set) -> Dict[str,int]:
     """ This function returns the number of views for each movie """
     views = {}
@@ -38,14 +24,32 @@ def get_recommendations(views: List, users_history: List) -> Dict[str, int]:
         if coincidence > len(user)//2:
             result |= { movie for movie in user if movie not in views }
 
+
     return get_views(result)
 
 
-# get all recommendations by user history
-recommendations = get_recommendations(user_history, history)
+if __name__ == '__main__':
 
-# get the most viewed recommendation
-recommendation_id = max(recommendations, key=recommendations.get)
-recommendation_name = all_movies[recommendation_id]
+    user_history = input().split(',')
 
-print(recommendation_name)
+    # get the history of others
+    with open('data/history.txt', 'r', encoding='UTF-8') as history_file:
+        history = [ user.split(',') for user in history_file.read().splitlines() ]
+
+
+    # get the movies
+    with open('data/movies.txt', 'r', encoding='UTF-8') as movies_file:
+        all_movies = [ movie.split(',') for movie in movies_file.read().splitlines() ]
+        all_movies = { movie[0]:movie[1] for movie in all_movies }
+
+
+    # get all recommendations by user history
+    recommendations = get_recommendations(user_history, history)
+
+    if len(recommendations):
+        # get the most viewed recommendation
+        recommendation_id = max(recommendations, key=recommendations.get)
+        recommendation_name = all_movies[recommendation_id]
+        print(recommendation_name)
+    else:
+        print('No any recommendations')
